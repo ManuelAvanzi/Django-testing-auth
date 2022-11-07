03315fff3288a511bacc6abe2b5537c8c91d7599
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-11-07 13:25:52 +0100

    add views : home & contatti

--- django_lv3/django_lv3/urls.py
+++ django_lv3/django_lv3/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from forms_app.views import contatti,homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homepage,name="home"),
+    path('contattaci/',contatti,name="contatti")
 ]
--- django_lv3/forms_app/views.py
+++ django_lv3/forms_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    return render(request,'forms_app/home.html')
+
+def contatti(request):
+    return render(request,'forms_app/contatto.html')
